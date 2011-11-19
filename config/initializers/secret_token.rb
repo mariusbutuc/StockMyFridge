48d6aad810eac59955b0212fb22f867750561d0d
Chris Lancaster <chris@whatimwear.in>
2011-11-19 01:06:28 -0500

    Initialized rails environment.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StockMyFridge::Application.config.secret_token = '42ec8d9a2b0d71e6f8528f312e1f3851679b784d44f55de475d913ebb04abb0b6eefcb733d9c72f1a0fe849e3bde1bdb8ed7de2774361b68b5ae4cb80e36d383'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+StockMyFridge::Application.config.session_store :cookie_store, :key => '_StockMyFridge_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# StockMyFridge::Application.config.session_store :active_record_store
