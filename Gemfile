e3d9163bf4da93228278a06a9232da8cf42630f9
Chris Lancaster <chris@whatimwear.in>
2011-11-19 12:54:19 -0500

    Added deploy script.

--- Gemfile
+++ Gemfile
@@ -1,8 +1,6 @@
 source 'http://rubygems.org'
 gem 'capistrano'
-gem 'cocaine'
 gem 'rails', '3.0.9'
 gem 'sqlite3'
-
 gem 'rest-client'
 gem 'nokogiri'
---
+++ config/deploy.rb
@@ -0,0 +1,15 @@
+set :application, "StokFridge"
+
+default_run_options[:pty] = true  # Must be set for the password prompt from git to work
+set :repository, "git@github.com:mariusbutuc/StockMyFridge.git"  # Your clone URL
+set :branch, "master"
+set :scm, "git"
+set :scm_verbose, true
+set :user, "root"              # The server's user for deploys
+set :scm_passphrase, "r3dhat^" # The deploy user's password
+
+role :web, "stokfridge.com"                   # Your HTTP server, Apache/etc
+role :app, "stokfridge.com"                   # This may be the same as your `Web` server
+role :db,  "stokfridge.com", :primary => true # This is where Rails migrations will run
+
+set :deploy_to, "/var/www/stokfridge"
